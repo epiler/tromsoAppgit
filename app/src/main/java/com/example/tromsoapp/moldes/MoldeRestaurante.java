74b4c7b686a5cc545073a5e19f4a2c2b095641f5
carlanmonsal <309@academicos.cesde.edu.co>
2023-08-08 20:57:23 -0500

    Listas

---
+++ app/src/main/java/com/example/tromsoapp/moldes/MoldeRestaurante.java
@@ -0,0 +1,59 @@
+package com.example.tromsoapp.moldes;public class MoldeRestaurante {
+
+private String nombre;
+private Integer foto;
+private String telefono;
+private String RangoPrecio;
+private String PlatoRecomendado;
+
+    public MoldeRestaurante() {
+    }
+
+    public MoldeRestaurante(String nombre, Integer foto, String telefono, String rangoPrecio, String platoRecomendado) {
+        this.nombre = nombre;
+        this.foto = foto;
+        this.telefono = telefono;
+        RangoPrecio = rangoPrecio;
+        PlatoRecomendado = platoRecomendado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Integer foto) {
+        this.foto = foto;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getRangoPrecio() {
+        return RangoPrecio;
+    }
+
+    public void setRangoPrecio(String rangoPrecio) {
+        RangoPrecio = rangoPrecio;
+    }
+
+    public String getPlatoRecomendado() {
+        return PlatoRecomendado;
+    }
+
+    public void setPlatoRecomendado(String platoRecomendado) {
+        PlatoRecomendado = platoRecomendado;
+    }
+}
